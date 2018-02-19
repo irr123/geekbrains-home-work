b806023069b9cc532be5e8a38553425bf5585df2
c1rno <ivan@bogomolov.su>
2018-02-19 22:25:54 +0700

    add non-blocking sockets

--- main.py
+++ main.py
@@ -7,15 +7,19 @@ import src
 def parce_params():
     parser = argparse.ArgumentParser(description='Socket example')
     parser.add_argument('mode', type=str, help='mode is `client` or `server`')
+    parser.add_argument(
+        '--blocking', type=int, default=1,
+        help='blocking or non-blocking sockets using in server, 1 or 0')
     return parser.parse_args()
 
 
 def main():
-    mode = parce_params().mode
+    args = parce_params()
+    print('Running with {}'.format(args))
     worker = {
         'client': src.socket_client.process,
-        'server': src.socket_server.process
-    }.get(mode, lambda: print('`{}`-mode not found'.format(mode)))
+        'server': lambda: src.socket_server.process(blocking=args.blocking)
+    }.get(args.mode, lambda: print('`{}`-mode not found'.format(args.mode)))
     worker()
 
 
--- src/socket_server.py
+++ src/socket_server.py
@@ -1,11 +1,13 @@
+import select
 import socket
 import time
 
 _is_sending_time = None
 
 
-def make_tcp_socket(address: str, port: int) -> socket.socket:
+def make_tcp_socket(address: str, port: int, blocking: int) -> socket.socket:
     socket_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_.setblocking(blocking)
     socket_.bind((address, port))
     socket_.listen(5)
     return socket_
@@ -20,12 +22,27 @@ def send_time(socket_: socket.socket) -> None:
         client.close()
 
 
-def process(address='', port=8888):
+def send_time_non_blocking(socket_: socket.socket) -> None:
+    while _is_sending_time:
+        is_ready = select.select((socket_,), tuple(), tuple(), 1)
+        if not is_ready[0]:
+            print('Not ready')
+            continue
+
+        client, address = socket_.accept()
+        print('Connected client from {}'.format(address))
+        payload = time.ctime(time.time()) + '\n'
+        client.send(payload.encode('ascii'))
+        client.close()
+
+
+def process(address='', port=8888, blocking=1):
     global _is_sending_time
     _is_sending_time = True
-    socket_ = make_tcp_socket(address, port)
+    socket_ = make_tcp_socket(address, port, blocking)
+    method = send_time if blocking == 1 else send_time_non_blocking
     try:
-        send_time(socket_)
+        method(socket_)
     except KeyboardInterrupt:
         _is_sending_time = False
         socket_.close()
